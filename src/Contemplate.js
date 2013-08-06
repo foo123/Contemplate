03f48241cd4292f39058a45876defba146fa11da
Nikos M <Nikos@Nikos-PC.(none)>
2013-08-06 13:30:36 +0300

    reduce some code

--- src/Contemplate.js
+++ src/Contemplate.js
@@ -766,229 +766,32 @@ function date (format, timestamp) {
   };
   return this.date(format, timestamp);
 }
-
-function localized_date (locale, format, timestamp) {
-    // localized version of phpjs date function
-    var that = this,
-      jsdate,
-      f,
-      formatChr = /\\?([a-z])/gi,
-      formatChrCb,
-      // Keep this here (works, but for code commented-out
-      // below for file size reasons)
-      //, tal= [],
-      _pad = function (n, c) {
-        n = n.toString();
-        return n.length < c ? _pad('0' + n, c, '0') : n;
-      },
-      txt_words = ["Sun", "Mon", "Tues", "Wednes", "Thurs", "Fri", "Satur", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  formatChrCb = function (t, s) {
-    return f[t] ? f[t]() : s;
-  };
-  f = {
-    // Day
-    d: function () { // Day of month w/leading 0; 01..31
-      return _pad(f.j(), 2);
-    },
-    D: function () { // Shorthand day name; Mon...Sun
-      var w=txt_words[f.w()];
-      return (locale[w]) ? locale[w] : w;
-    },
-    j: function () { // Day of month; 1..31
-      return jsdate.getDate();
-    },
-    l: function () { // Full day name; Monday...Sunday
-      var w=txt_words[f.w()] + 'day';
-      return (locale[w]) ? locale[w] : w;
-    },
-    N: function () { // ISO-8601 day of week; 1[Mon]..7[Sun]
-      return f.w() || 7;
-    },
-    S: function () { // Ordinal suffix for day of month; st, nd, rd, th
-      var j = f.j();
-      var w=(['st', 'nd', 'rd'][j % 10 - 1] || 'th');
-      var l= (locale[w]) ? locale[w] : w;
-      return j < 4 | j > 20 && l;
-    },
-    w: function () { // Day of week; 0[Sun]..6[Sat]
-      return jsdate.getDay();
-    },
-    z: function () { // Day of year; 0..365
-      var a = new Date(f.Y(), f.n() - 1, f.j()),
-        b = new Date(f.Y(), 0, 1);
-      return Math.round((a - b) / 864e5);
-    },
-
-    // Week
-    W: function () { // ISO-8601 week number
-      var a = new Date(f.Y(), f.n() - 1, f.j() - f.N() + 3),
-        b = new Date(a.getFullYear(), 0, 4);
-      return _pad(1 + Math.round((a - b) / 864e5 / 7), 2);
-    },
-
-    // Month
-    F: function () { // Full month name; January...December
-      var w=txt_words[6 + f.n()];
-      return (locale[w]) ? locale[w] : w;
-    },
-    m: function () { // Month w/leading 0; 01...12
-      return _pad(f.n(), 2);
-    },
-    M: function () { // Shorthand month name; Jan...Dec
-      var w=txt_words[6 + f.n()].slice(0, 3);
-      return (locale[w]) ? locale[w] : w;
-    },
-    n: function () { // Month; 1...12
-      return jsdate.getMonth() + 1;
-    },
-    t: function () { // Days in month; 28...31
-      return (new Date(f.Y(), f.n(), 0)).getDate();
-    },
-
-    // Year
-    L: function () { // Is leap year?; 0 or 1
-      var j = f.Y();
-      return j % 4 === 0 & j % 100 !== 0 | j % 400 === 0;
-    },
-    o: function () { // ISO-8601 year
-      var n = f.n(),
-        W = f.W(),
-        Y = f.Y();
-      return Y + (n === 12 && W < 9 ? 1 : n === 1 && W > 9 ? -1 : 0);
-    },
-    Y: function () { // Full year; e.g. 1980...2010
-      return jsdate.getFullYear();
-    },
-    y: function () { // Last two digits of year; 00...99
-      return f.Y().toString().slice(-2);
-    },
-
-    // Time
-    a: function () { // am or pm
-      var lam=(locale['am']) ? locale['am'] : 'am', lpm=(locale['pm']) ? locale['pm'] : 'pm';
-      return jsdate.getHours() > 11 ? lpm : lam;
-    },
-    A: function () { // AM or PM
-      return f.a().toUpperCase();
-    },
-    B: function () { // Swatch Internet time; 000..999
-      var H = jsdate.getUTCHours() * 36e2,
-        // Hours
-        i = jsdate.getUTCMinutes() * 60,
-        // Minutes
-        s = jsdate.getUTCSeconds(); // Seconds
-      return _pad(Math.floor((H + i + s + 36e2) / 86.4) % 1e3, 3);
-    },
-    g: function () { // 12-Hours; 1..12
-      return f.G() % 12 || 12;
-    },
-    G: function () { // 24-Hours; 0..23
-      return jsdate.getHours();
-    },
-    h: function () { // 12-Hours w/leading 0; 01..12
-      return _pad(f.g(), 2);
-    },
-    H: function () { // 24-Hours w/leading 0; 00..23
-      return _pad(f.G(), 2);
-    },
-    i: function () { // Minutes w/leading 0; 00..59
-      return _pad(jsdate.getMinutes(), 2);
-    },
-    s: function () { // Seconds w/leading 0; 00..59
-      return _pad(jsdate.getSeconds(), 2);
-    },
-    u: function () { // Microseconds; 000000-999000
-      return _pad(jsdate.getMilliseconds() * 1000, 6);
-    },
-
-    // Timezone
-    e: function () { // Timezone identifier; e.g. Atlantic/Azores, ...
-      // The following works, but requires inclusion of the very large
-      // timezone_abbreviations_list() function.
-/*              return that.date_default_timezone_get();
-*/
-      throw 'Not supported (see source code of date() for timezone on how to add support)';
-    },
-    I: function () { // DST observed?; 0 or 1
-      // Compares Jan 1 minus Jan 1 UTC to Jul 1 minus Jul 1 UTC.
-      // If they are not equal, then DST is observed.
-      var a = new Date(f.Y(), 0),
-        // Jan 1
-        c = Date.UTC(f.Y(), 0),
-        // Jan 1 UTC
-        b = new Date(f.Y(), 6),
-        // Jul 1
-        d = Date.UTC(f.Y(), 6); // Jul 1 UTC
-      return ((a - c) !== (b - d)) ? 1 : 0;
-    },
-    O: function () { // Difference to GMT in hour format; e.g. +0200
-      var tzo = jsdate.getTimezoneOffset(),
-        a = Math.abs(tzo);
-      return (tzo > 0 ? "-" : "+") + _pad(Math.floor(a / 60) * 100 + a % 60, 4);
-    },
-    P: function () { // Difference to GMT w/colon; e.g. +02:00
-      var O = f.O();
-      return (O.substr(0, 3) + ":" + O.substr(3, 2));
-    },
-    T: function () { // Timezone abbreviation; e.g. EST, MDT, ...
-      // The following works, but requires inclusion of the very
-      // large timezone_abbreviations_list() function.
-/*              var abbr = '', i = 0, os = 0, default = 0;
-      if (!tal.length) {
-        tal = that.timezone_abbreviations_list();
-      }
-      if (that.php_js && that.php_js.default_timezone) {
-        default = that.php_js.default_timezone;
-        for (abbr in tal) {
-          for (i=0; i < tal[abbr].length; i++) {
-            if (tal[abbr][i].timezone_id === default) {
-              return abbr.toUpperCase();
-            }
-          }
-        }
-      }
-      for (abbr in tal) {
-        for (i = 0; i < tal[abbr].length; i++) {
-          os = -jsdate.getTimezoneOffset() * 60;
-          if (tal[abbr][i].offset === os) {
-            return abbr.toUpperCase();
-          }
-        }
-      }
-*/
-      return 'UTC';
-    },
-    Z: function () { // Timezone offset in seconds (-43200...50400)
-      return -jsdate.getTimezoneOffset() * 60;
-    },
-
-    // Full Date/Time
-    c: function () { // ISO-8601 date.
-      return 'Y-m-d\\TH:i:sP'.replace(formatChr, formatChrCb);
-    },
-    r: function () { // RFC 2822
-      return 'D, d M Y H:i:s O'.replace(formatChr, formatChrCb);
-    },
-    U: function () { // Seconds since UNIX epoch
-      return jsdate / 1000 | 0;
+function localized_date($locale, $format, $timestamp) 
+{
+    var $txt_words = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sun", "Mon", "Tues", "Wednes", "Thurs", "Fri", "Satur", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var $am_pm=['AM', 'PM', 'am', 'PM'];
+    
+    var $date=date($format, $timestamp);
+    
+    // localize days/months
+    for (var i=0, l=$txt_words.length; i<l; i++)
+    {
+        if ($locale[$txt_words[i]])  $date=$date.replace($txt_words[i], $locale[$txt_words[i]]);
     }
-  };
-  this.date = function (format, timestamp) {
-    that = this;
-    jsdate = (timestamp === undefined ? new Date() : // Not provided
-      (timestamp instanceof Date) ? new Date(timestamp) : // JS Date()
-      new Date(timestamp * 1000) // UNIX timestamp (auto-convert to int)
-    );
-    return format.replace(formatChr, formatChrCb);
-  };
-  return this.date(format, timestamp);
+    // localize am/pm
+    for (var i=0, l=$am_pm.length; i<l; i++)
+    {
+        if ($locale[$am_pm[i]])  $date=$date.replace($am_pm[i], $locale[$am_pm[i]]);
+    }
+    // return localized date
+    return $date;
 }
     
     // private vars
     var 
         $escaper=null,
         
-        $__cacheMode=0, $_thisPath=null,
+        $__cacheMode=0,
         
         $__cache={}, $__templates={}, $__partials={},
         $__locale={},
@@ -1056,8 +859,7 @@ function localized_date (locale, format, timestamp) {
         CACHE_TO_DISK_NOUPDATE : 4,
         
         init : function(escaper) {
-            if (escaper)
-                $escaper=escaper;
+            if (escaper)  $escaper=escaper;
             
             // pre-compute the needed regular expressions
             $regExps['controlConstructs']=new RegExp('\\t\\s*\%('+$controlConstructs.join('|')+')\\b\\s*\\((.*)\\)', 'g');
@@ -1093,12 +895,10 @@ function localized_date (locale, format, timestamp) {
             {
                 // filesystem I/O object of Nodejs
                 self.NFS = $fs || require('fs');
-                $_thisPath = __filename;
             }
             else
             {
                 self.NFS = null;
-                $_thisPath = null;
             }
         },
         
@@ -1567,77 +1367,41 @@ function localized_date (locale, format, timestamp) {
         
         createTemplateRenderFunction : function($id) {
             self.reset();
-            if (self.IS_NODEJS)
-            {
-                var $func=
-                    // use php-style variables using '$' in front of var name
-                    "for (var __n__ in $__o__) { if (Contemplate.hasOwn($__o__, __n__)) { $__o__['$'+__n__]=$__o__[__n__]; delete $__o__[__n__];} } "
-                    // Introduce the data as local variables using with(){}
-                   // Convert the template into pure JavaScript
-                    +"var $__p__ = ''; "
-                    +"with($__o__) { $__p__ += '" + self.parse(self.getTemplateContents($id)) + "'; } "
-                    +"return $__p__;"
-                    ;
-            }
-            else
-            {
-                var $func=
-                    // use php-style variables using '$' in front of var name
-                    "for (var __n__ in $__o__) { if (Contemplate.hasOwn($__o__, __n__)) { $__o__['$'+__n__]=$__o__[__n__]; delete $__o__[__n__];} } "
-                    // Introduce the data as local variables using with(){}
-                   // Convert the template into pure JavaScript
-                    +"var $__p__ = ''; "
-                    +"with($__o__) { $__p__ += '" + self.parse(self.getTemplateContents($id)) + "'; } "
-                    +"return $__p__;"
-                    ;
-            }
+            var $func=
+                // use php-style variables using '$' in front of var name
+                "for (var __n__ in $__o__) { if (Contemplate.hasOwn($__o__, __n__)) { $__o__['$'+__n__]=$__o__[__n__]; delete $__o__[__n__];} } "
+                // Introduce the data as local variables using with(){}
+               // Convert the template into pure JavaScript
+                +"var $__p__ = ''; "
+                +"with($__o__) { $__p__ += '" + self.parse(self.getTemplateContents($id)) + "'; } "
+                +"return $__p__;"
+                ;
             return new Function("$__o__", $func);
         },
         
         createCachedTemplate : function($id, $filename, $classname) {
             self.reset();
-        
-            if (self.IS_NODEJS)
-            {
-                var $class=
-                    "(function(root) { " + "\n"
-                    +"/* Contemplate cached template '"+$id+"' */ " + "\n"
-                    +"function " + $classname + "($id) { this.id=$id; }; "
-                    +$classname + ".prototype.setId=function($id) { if ($id) {this.id=$id;} return this; }; "
-                    +$classname + ".prototype.render=function($__o__) { "
-                    // use php-style variables using '$' in front of var name
-                    +"for (var __n__ in $__o__) { if (Contemplate.hasOwn($__o__, __n__)) { $__o__['$'+__n__]=$__o__[__n__]; delete $__o__[__n__];} } "
-                    // Introduce the data as local variables using with(){}
-                   // Convert the template into pure JavaScript
-                    +"var $__p__ = ''; "
-                    +"with($__o__) { $__p__ += '" + self.parse(self.getTemplateContents($id)) + "'; } "
-                    +"return $__p__; "
-                    +"}; if ('undefined' != typeof (module) && module.exports) {module.exports="+$classname+";} else if (typeof (exports) != 'undefined') {exports="+$classname+";}  else {root." + $classname + "="+$classname+";} })(this);"
-                    ;
-            }
-            else
-            {
-                var $class=
-                    "(function(root) { " + "\n"
-                    +"/* Contemplate cached template '"+$id+"' */ " + "\n"
-                    +"function " + $classname + "($id) { this.id=$id; }; "
-                    +$classname + ".prototype.setId=function($id) { if ($id) {this.id=$id;} return this; }; "
-                    +$classname + ".prototype.render=function($__o__) { "
-                    // use php-style variables using '$' in front of var name
-                    +"for (var __n__ in $__o__) { if (Contemplate.hasOwn($__o__, __n__)) { $__o__['$'+__n__]=$__o__[__n__]; delete $__o__[__n__];} } "
-                    // Introduce the data as local variables using with(){}
-                   // Convert the template into pure JavaScript
-                    +"var $__p__ = ''; "
-                    +"with($__o__) { $__p__ += '" + self.parse(self.getTemplateContents($id)) + "'; } "
-                    +"return $__p__; "
-                    +"}; if ('undefined' != typeof (module) && module.exports) {module.exports="+$classname+";} else if (typeof (exports) != 'undefined') {exports="+$classname+";}  else {root." + $classname + "="+$classname+";} })(this);"
-                    ;
-            }
+            var $class=
+                "(function(root) { " + "\n"
+                +"/* Contemplate cached template '"+$id+"' */ " + "\n"
+                +"function " + $classname + "($id) { this.id=$id; }; "
+                +$classname + ".prototype.setId=function($id) { if ($id) {this.id=$id;} return this; }; "
+                +$classname + ".prototype.render=function($__o__) { "
+                // use php-style variables using '$' in front of var name
+                +"for (var __n__ in $__o__) { if (Contemplate.hasOwn($__o__, __n__)) { $__o__['$'+__n__]=$__o__[__n__]; delete $__o__[__n__];} } "
+                // Introduce the data as local variables using with(){}
+               // Convert the template into pure JavaScript
+                +"var $__p__ = ''; "
+                +"with($__o__) { $__p__ += '" + self.parse(self.getTemplateContents($id)) + "'; } "
+                +"return $__p__; "
+                +"}; if ('undefined' != typeof (module) && module.exports) {module.exports="+$classname+";} else if (typeof (exports) != 'undefined') {exports="+$classname+";}  else {root." + $classname + "="+$classname+";} })(this);"
+                ;
             return self.setCachedTemplate($filename, $class);
         },
         
         getCachedTemplate : function($id) {
             
+            if (!self.IS_NODEJS) $__cacheMode=self.CACHE_TO_DISK_NONE;
             switch($__cacheMode)
             {
                 case self.CACHE_TO_DISK_NOUPDATE:

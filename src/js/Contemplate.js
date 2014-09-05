27487a937e4afeb52448efee5a1e645c73d0d967
Nikos M <nikosms0@gmail.com>
2014-09-06 00:13:51 +0300

    update buildtools, dependencies

--- src/js/Contemplate.js
+++ src/js/Contemplate.js
@@ -10,25 +10,28 @@
 *
 **/
 !function( root, name, factory ) {
-
     "use strict";
     
     //
-    // export the module, umd-style
+    // export the module, umd-style (no other dependencies)
+    var isCommonJS = ("object" === typeof(module)) && module.exports, 
+        isAMD = ("function" === typeof(define)) && define.amd, m;
     
-    // node, CommonJS, etc..
-    if ( 'object' === typeof(module) && module.exports ) 
-        module.exports = (module.deps = module.deps || {})[ name ] = module.deps[ name ] || (factory.call( root ) || 1);
+    // CommonJS, node, etc..
+    if ( isCommonJS ) 
+        module.exports = (module.$deps = module.$deps || {})[ name ] = module.$deps[ name ] || (factory.call( root, {NODE:module} ) || 1);
     
-    // AMD, etc..
-    else if ( 'function' === typeof(define) && define.amd ) define( name, [ ], function( ){ return factory.call( root ); } );
+    // AMD, requireJS, etc..
+    else if ( isAMD && ("function" === typeof(require)) && ("function" === typeof(require.specified)) && require.specified(name) ) 
+        define( name, ['require', 'exports', 'module'], function( require, exports, module ){ return factory.call( root, {AMD:module} ); } );
     
-    // browser, etc..
-    else if ( !(name in root) ) root[ name ] = factory.call( root ) || 1;
+    // browser, web worker, etc.. + AMD, other loaders
+    else if ( !(name in root) ) 
+        (root[ name ] = (m=factory.call( root, {} ) || 1)) && isAMD && define( name, [], function( ){ return m; } );
 
 
 }(  /* current root */          this, 
-    /* module name */           'Contemplate',
+    /* module name */           "Contemplate",
     /* module factory */        function( undef ) {
         
     "use strict";
